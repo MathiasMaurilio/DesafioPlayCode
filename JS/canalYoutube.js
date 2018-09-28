ab4e5c6a28f60dae8dd9292f6f8d0fe02c136065
Mathias S Maurilio <maurilio.1996.mathias@gmail.com>
2018-09-28 15:20:59 -0300

    created the methods to work with youtube api

--- JS/canalYoutube.js
+++ JS/canalYoutube.js
@@ -1,4 +1,5 @@
 class CanalYoutube {
+
     static getVideos(){
         var listaVideos = [];
         var primeiroVideo = new Video("imagem1", "link1");
@@ -8,7 +9,33 @@ class CanalYoutube {
 
         return listaVideos;
     }
+
+    static getIdVideosFromAPI(apiUrl){
+        var listaVideosId = [];
+        $.getJSON(apiUrl, function(resultado){
+            resultado.items.forEach(function(video){
+                listaVideosId.push(video.id.videoId);
+            });
+        });
+
+        return listaVideosId;
+    }
+
+    static getImageVideoFromAPI(videoId, apiUrl, video){
+        var videoImagem = "";
+
+        $.getJSON(apiUrl, function(resultado){
+            videoImagem = resultado.items[0].snippet.thumbnails.medium.url;
+            video.image = videoImagem;
+        });
+        return videoImagem;
+    }
 }
 
+var jsonGeral = "https://api.myjson.com/bins/ec9qf";
+var jsonVideo = "https://api.myjson.com/bins/1e4f6v";
+
 var listaVideos = CanalYoutube.getVideos();
-console.log(listaVideos);
+var listaIdVideos = CanalYoutube.getIdVideosFromAPI(jsonGeral);
+var video = new Video("Teste", "Teste");
+var imagem = CanalYoutube.getImageVideoFromAPI("teste", jsonVideo, video);
