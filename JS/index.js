bbee19e8d420bae5389bddfc987ca0b57c17df8f
Mathias S Maurilio <maurilio.1996.mathias@gmail.com>
2019-01-13 22:56:08 -0200

    using youtube api
    swapping static json through the YouTube API

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,11 @@
-var jsonGeral = "https://api.myjson.com/bins/ec9qf";
-var jsonVideo = "https://api.myjson.com/bins/1e4f6v";
+var apiYoutubePrimeira = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCTJ1mLre8sT-d4KuvmQsSQA&maxResults=50&type=video&fields=nextPageToken%2Citems(id%2Csnippet(thumbnails(medium)))&key=AIzaSyDVG1UxvcC87C-AKmudaMIpbgw9XRTeD1M";
+var apiYoutubeSegunda = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCTJ1mLre8sT-d4KuvmQsSQA&maxResults=50&pageToken=CDIQAA&type=video&fields=nextPageToken%2Citems(id%2Csnippet(thumbnails(medium)))&key=AIzaSyDVG1UxvcC87C-AKmudaMIpbgw9XRTeD1M";
 var tr = $(".video-coluna");
 
-var listaVideos = VideosDoYoutube.inicializaVideos(jsonGeral);
+var listaVideos = VideosDoYoutube.inicializaVideos();
+VideosDoYoutube.getVideosFromAPI(apiYoutubePrimeira, listaVideos);
+VideosDoYoutube.getVideosFromAPI(apiYoutubeSegunda, listaVideos);
+
 InicializarTds(listaVideos);
 console.log(listaVideos);
 
